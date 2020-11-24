7a99f217af1404ea651c2b80e3a67cda7063c02c
Alexia Kattah <alexia.kattah@oqvocequer.com.br>
2020-11-24 08:07:30 -0300

    site

---
+++ src/portfolio.js
@@ -0,0 +1,359 @@
+﻿/* Change this file to get your personal Portfolio */
+
+// Your Summary And Greeting Section
+
+import emoji from 'react-easy-emoji';
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  username: 'Alexia Kattah',
+  title: 'Olá, eu sou a Alexia',
+  subTitle: emoji(
+    'Engenheira de Computação, desenvolvedora de software e CTO 🚀 Apaixonada por tecnologia, e com experiência na criação de aplicativos Web/mobile com Javascript/ React/ Node/React Native e algumas outras bibliotecas interessantes.'
+  ),
+  resumeLink:
+    'https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing',
+};
+
+// Your Social Media Link
+
+const socialMediaLinks = {
+  github: 'https://github.com/alexiakattah',
+  linkedin: 'https://www.linkedin.com/in/alexiakattah/',
+  gmail: 'eng.alexiakattah@gmail.com',
+  // gitlab: 'https://gitlab.com/saadpasta',
+  facebook: 'https://www.facebook.com/alexia.karoline',
+  instagram: 'https://www.instagram.com/alexiakattah',
+  // medium: 'https://medium.com/@saadpasta',
+  // stackoverflow: 'https://stackoverflow.com/users/10422806/saad-pasta',
+  // Instagram and Twitter are also supported in the links!
+};
+
+// Your Skills Section
+
+const skillsSection = {
+  title: 'Sobre mim',
+  // subTitle: 'CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK',
+  skills: [
+    emoji(
+      '⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications'
+    ),
+    emoji('⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks'),
+    emoji(
+      '⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean'
+    ),
+  ],
+
+  /* Make Sure You include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: 'html-5',
+      fontAwesomeClassname: 'fab fa-html5',
+    },
+    {
+      skillName: 'css3',
+      fontAwesomeClassname: 'fab fa-css3-alt',
+    },
+
+    {
+      skillName: 'sass',
+      fontAwesomeClassname: 'fab fa-sass',
+    },
+    {
+      skillName: 'JavaScript',
+      fontAwesomeClassname: 'fab fa-js',
+    },
+    {
+      skillName: 'reactjs',
+      fontAwesomeClassname: 'fab fa-react',
+    },
+    {
+      skillName: 'nodejs',
+      fontAwesomeClassname: 'fab fa-node',
+    },
+    {
+      skillName: 'react native',
+      fontAwesomeClassname: 'fab fa-react',
+    },
+    {
+      skillName: 'npm',
+      fontAwesomeClassname: 'fab fa-npm',
+    },
+    {
+      skillName: 'SQL',
+      fontAwesomeClassname: 'fas fa-database',
+    },
+    {
+      skillName: 'aws',
+      fontAwesomeClassname: 'fab fa-aws',
+    },
+    {
+      skillName: 'firebase',
+      fontAwesomeClassname: 'fas fa-fire',
+    },
+    {
+      skillName: 'docker',
+      fontAwesomeClassname: 'fab fa-docker',
+    },
+  ],
+};
+
+// Your education background
+
+const educationInfo = {
+  viewEducation: true, // Set it to true to see education section
+  schools: [
+    {
+      schoolName: 'Universidade do Estado de Minas Gerais',
+      logo: require('./assets/images/Uemg.jpg'),
+      subHeader: 'Engenharia de Computação',
+      duration: 'Agosto 2015 - Agosto 2020',
+      desc: 'Participated in the research of XXX and published 3 papers.',
+      descBullets: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      ],
+    },
+    {
+      schoolName: 'Rocketseat',
+      logo: require('./assets/images/rocket.jpg'),
+      subHeader: 'GoStack Bootcamp',
+      duration: 'Janeiro 2020 - Março 2020',
+      desc:
+        'Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...',
+      descBullets: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit'],
+    },
+  ],
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: 'Frontend/Design', //Insert stack or technology you have experience in
+      progressPercentage: '90%', //Insert relative proficiency in percentage
+    },
+    {
+      Stack: 'Backend',
+      progressPercentage: '70%',
+    },
+    {
+      Stack: 'UI/UX', //Insert stack or technology you have experience in
+      progressPercentage: '60%', //Insert relative proficiency in percentage
+    },
+    {
+      Stack: 'Programming',
+      progressPercentage: '60%',
+    },
+  ],
+};
+
+// Your top 3 work experiences
+
+const workExperiences = {
+  viewExperiences: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: 'CTO',
+      company: 'OQ Digital',
+      companylogo: require('./assets/images/0.jpeg'),
+      date: 'Março 2020 – Atual',
+      desc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      // descBullets: [
+      //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      // ],
+    },
+    {
+      role: 'UX Design',
+      company: 'Grupo Sym',
+      companylogo: require('./assets/images/0.png'),
+      date: 'Outubro 2018 – Janeiro 2019',
+      desc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      role: 'Fullstack Developer',
+      company: 'Freelance',
+      companylogo: require('./assets/images/freelance.png'),
+      date: 'Jan 2015 – Sep 2015',
+      desc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+  ],
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: 'alexiakattah', // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: 'true', // Set true or false to show Contact profile using Github, defaults to false
+};
+
+// Some Big Projects You have worked with your company
+
+const bigProjects = {
+  title: 'Projetos',
+  // subtitle: 'SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH',
+  projects: [
+    {
+      image: require('./assets/images/saayaHealthLogo.webp'),
+      link: 'http://saayahealth.com/',
+    },
+    {
+      image: require('./assets/images/nextuLogo.webp'),
+      link: 'http://nextu.se/',
+    },
+  ],
+};
+
+// Your Achievement Section Include Your Certification Talks and More
+
+const achievementSection = {
+  title: emoji('Conquistas e Certificados 🏆 '),
+  subtitle:
+    'Conquistas, certificações, cartas de premiação e algumas coisas legais!',
+
+  achievementsCards: [
+    {
+      title: 'Google Code-In Finalist',
+      subtitle:
+        'First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.',
+      image: require('./assets/images/codeInLogo.webp'),
+      footerLink: [
+        {
+          name: 'Certification',
+          url:
+            'https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing',
+        },
+        {
+          name: 'Award Letter',
+          url:
+            'https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing',
+        },
+        {
+          name: 'Google Code-in Blog',
+          url:
+            'https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html',
+        },
+      ],
+    },
+    {
+      title: 'Google Assistant Action',
+      subtitle:
+        'Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.',
+      image: require('./assets/images/googleAssistantLogo.webp'),
+      footerLink: [
+        {
+          name: 'View Google Assistant Action',
+          url:
+            'https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en',
+        },
+      ],
+    },
+
+    {
+      title: 'PWA Web App Developer',
+      subtitle: 'Completed Certifcation from SMIT for PWA Web App Development',
+      image: require('./assets/images/pwaLogo.webp'),
+      footerLink: [
+        { name: 'Certification', url: '' },
+        {
+          name: 'Final Project',
+          url: 'https://pakistan-olx-1.firebaseapp.com/',
+        },
+      ],
+    },
+  ],
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: 'Blogs',
+  subtitle:
+    'With Love for Developing cool stuff, I love to write and teach others what I have learnt.',
+
+  blogs: [
+    {
+      url:
+        'https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae',
+      title: 'Win a Google Assistant Tshirt and $200 in Google Cloud Credits',
+      description:
+        'Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?',
+    },
+    {
+      url: 'https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e',
+      title: 'Why REACT is The Best?',
+      description:
+        'React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies.',
+    },
+  ],
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: 'TALKS',
+  subtitle: emoji(
+    'I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅'
+  ),
+
+  talks: [
+    {
+      title: 'Build Actions For Google Assistant',
+      subtitle: 'Codelab at GDG DevFest Karachi 2019',
+      slides_url: 'https://bit.ly/saadpasta-slides',
+      event_url: 'https://www.facebook.com/events/2339906106275053/',
+    },
+  ],
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji('Podcast 🎙️'),
+  subtitle: 'I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY',
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    'https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo',
+  ],
+};
+
+const contactInfo = {
+  title: emoji('Contato ☎️'),
+  subtitle:
+    'Tem algum projeto e quer desenvolver ele? Entre em contato comigo.',
+  number: '(37) 99905-9194',
+  email_address: 'eng.alexiakattah@gmail.com',
+};
+
+//Twitter Section
+
+const twitterDetails = {
+  userName: 'alexiakattah', //Replace "twitter" with your twitter username without @
+};
+export {
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+};
